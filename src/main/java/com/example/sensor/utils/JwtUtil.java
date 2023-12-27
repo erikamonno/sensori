95df56eaa045395275c00906a3d32a2bc8f9fe75
Marco Amoia <marco.amoia9@gmail.com>
2023-12-27 14:42:51 +0100

    changed jwt library

--- src/main/java/com/example/sensor/utils/JwtUtil.java
+++ src/main/java/com/example/sensor/utils/JwtUtil.java
@@ -1,22 +1,41 @@
 package com.example.sensor.utils;
 
 import com.example.sensor.model.entities.Utente;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.proc.BadJOSEException;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.Key;
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -25,82 +44,64 @@ public class JwtUtil {
     @Value("${jwt.secret-key}")
     private String secretKeyPath;
     private byte[] secretKey;
-    private long ONE_HOUR_IN_MILLIS = 60*60*1000;
 
-    private JwtParser jwtParser;
+    @Value("${jwt.issuer}")
+    private String issuer;
 
-    private final String TOKEN_HEADER = "Authorization";
-    private final String TOKEN_PREFIX = "Bearer ";
+    @Value("${jwt.expire-hours}")
+    Long expireHourToken;
+
+    private static final String SECRET = "FBA898697394CDBC534E7ED86A97AA59F627FE6B309E0A21EEC6C9B130E0369C";
 
     @PostConstruct
-    private void initialize() throws IOException {
-        log.info("JwtUtil.initialize()");
-        this.secretKey = null;
-        this.secretKey = Files.readAllBytes(Path.of(secretKeyPath));
-        this.jwtParser = Jwts.parser().setSigningKey(getSigningKey()).build();
+    private void init() throws IOException {
+        secretKey = Files.readAllBytes(Path.of(getClass().getClassLoader().getResource(secretKeyPath).getPath()));
     }
 
     public String createToken(Utente user) {
-        Date tokenCreateTime = new Date();
-        Date tokenValidity = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(ONE_HOUR_IN_MILLIS));
-        Claims claims = Jwts.claims()
-                .subject(user.getUsername())
-                .add("firstName",user.getNome())
-                .add("lastName",user.getCognome())
-                .add("roles", user.getAdmin() ? RoleConstants.ADMIN : RoleConstants.USER)
-                .build();
-        return Jwts.builder()
-                .claims(claims)
-                .expiration(tokenValidity)
-                .signWith(getSigningKey())
-                .compact();
-    }
-    private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(secretKey);
-    }
-    private Claims parseJwtClaims(String token) {
-        return jwtParser.parseClaimsJws(token).getBody();
-    }
-
-    public Claims resolveClaims(HttpServletRequest req) {
         try {
-            String token = resolveToken(req);
-            if (token != null) {
-                return parseJwtClaims(token);
-            }
-            return null;
-        } catch (ExpiredJwtException ex) {
-            req.setAttribute("expired", ex.getMessage());
-            throw ex;
-        } catch (Exception ex) {
-            req.setAttribute("invalid", ex.getMessage());
-            throw ex;
+            JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                    .issuer(issuer)
+                    .subject(user.getUsername())
+                    .claim("firstName", user.getNome())
+                    .claim("lastName", user.getCognome())
+                    .claim("roles", List.of(user.getAdmin() ? RoleConstants.ADMIN : RoleConstants.USER))
+                    .expirationTime(Date.from(LocalDateTime.now().plusHours(expireHourToken).atZone(ZoneId.systemDefault()).toInstant()))
+                    .issueTime(new Date())
+                    .build();
+
+            Payload payload = new Payload(claims.toJSONObject());
+
+            JWSObject jwsObject = new JWSObject(new JWSHeader(JWSAlgorithm.HS256),
+                    payload);
+
+            jwsObject.sign(new MACSigner(SECRET));
+            return jwsObject.serialize();
+        }
+        catch (JOSEException e) {
+            throw new RuntimeException("Error to create JWT", e);
         }
     }
 
-    public String resolveToken(HttpServletRequest request) {
+    public UsernamePasswordAuthenticationToken parseToken(String token) throws JOSEException, ParseException,
+            BadJOSEException {
 
-        String bearerToken = request.getHeader(TOKEN_HEADER);
-        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
-            return bearerToken.substring(TOKEN_PREFIX.length());
-        }
-        return null;
-    }
+        byte[] secretKey = SECRET.getBytes();
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        signedJWT.verify(new MACVerifier(secretKey));
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
 
-    public boolean validateClaims(Claims claims) throws AuthenticationException {
-        try {
-            return claims.getExpiration().after(new Date());
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
-    public String getEmail(Claims claims) {
-        return claims.getSubject();
-    }
-
-    private List<String> getRoles(Claims claims) {
-        return (List<String>) claims.get("roles");
+        JWSKeySelector<SecurityContext> keySelector = new JWSVerificationKeySelector<>(JWSAlgorithm.HS256,
+                new ImmutableSecret<>(secretKey));
+        jwtProcessor.setJWSKeySelector(keySelector);
+        jwtProcessor.process(signedJWT, null);
+        JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
+        String username = claims.getSubject();
+        var roles = (List<String>) claims.getClaim("roles");
+        var authorities = roles == null ? null : roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new UsernamePasswordAuthenticationToken(username, null, authorities);
     }
 
 
